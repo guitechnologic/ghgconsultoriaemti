695ca23df8f299da99e35a375c45c8509c0920d1
gui.technologic <guilherme@localhost.localdomain>
2022-09-12 16:43:03 -0300

    atualizaçãoGit

---
+++ Templates/CafeTemplate/script.js
@@ -0,0 +1,15 @@
+	// Tabbed Menu
+    function openMenu(evt, menuName) {
+        var i, x, tablinks;
+            x = document.getElementsByClassName("menu");
+			for (i = 0; i < x.length; i++) {
+					x[i].style.display = "none";
+				}
+				tablinks = document.getElementsByClassName("tablink");
+				for (i = 0; i < x.length; i++) {
+					tablinks[i].className = tablinks[i].className.replace(" dark-grey", "");
+				}
+				document.getElementById(menuName).style.display = "block";
+				evt.currentTarget.firstElementChild.className += " dark-grey";
+			}
+			document.getElementById("myLink").click();
