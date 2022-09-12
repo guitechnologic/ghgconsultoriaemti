c6a17a0b701db57f62293e7b137ad464aa52bad0
guitechnologic <39844853+guitechnologic@users.noreply.github.com>
2022-09-12 16:37:22 -0300

    Add files via upload

---
+++ Menu/menu.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+    <html>
+        <title> GHG Consultoria Em Ti   </title>
+
+            <meta   charset="UTF-8">
+            <meta   name="viewport"                 content="width=device-width,    initial-scale=1">
+            <meta   http-equiv="X-UA-Compatible"    content="IE-edge">
+            <meta   name="geo.region"               content="BR-PR">
+            <meta   name="geo.placement"            content="Colombo">
+            <meta   name="geo.position"             content="-25.36997746782584, -49.16598553283551">
+            <meta   name="ICBM"                     content="-25.36997746782584, -49.16598553283551">
+            <link   rel="icon"                      href="../_imagens/logovetorizadaghg/GHG - Consultoria em TI 2.png" sizes="16x16" type="image/png">
+            <link   rel="stylesheet"                href="../_css/estilo.css">
+            <link   rel="stylesheet"                href="https://fonts.googleapis.com/css?family=Raleway">
+            <link   rel="stylesheet"                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   <!-- Icones Minimalistas -->
+
+    <body>
+        
+        <div    class="top">
+            <div    class="bar  white card" id="myNavbar">
+                <a  href="#Home"    class="bar-item button  wide">  GHG </a>
+
+                <!-- 01.1 - Botoes de navegação -->
+                <div    class="center    hide-small">
+                    <a  href="#Home"                    class="bar-item button">        <i class="fa fa-home">      </i> HOME           </a>
+                    <a  href="#servicos"                class="bar-item button">        <i class="fa fa-th">        </i> SERVIÇOS       </a>
+                    <a  href="SobreNos/index.html"      class="bar-item button">        <i class="fa fa-user">      </i> SOBRE NÓS      </a>
+                    <a  href="#Contato"                 class="bar-item button">        <i class="fa fa-envelope">  </i> CONTATO        </a>
+                    <a  href="indexEnglish.html"        class="bar-item button right">  <i class="fa fa-globe">     </i> ENGLISH        </a>
+                </div>
+                
+                <!-- 01.2 - Opçoes do menu de navegaçao para mobile, abertura e fechamento -->
+                <a  href="javascript:void(0)"   class="bar-item button  center   hide-large  hide-medium"    onclick="botaoAbre()">
+                <i  class="fa   fa-bars"> </i>
+                </a>
+            </div>
+        </div>
+
+        <nav    class="sidebar  bar-block   black   card    animate-left    hide-medium hide-large" style="display:none"    id="mySidebar">
+            <a  href="javascript:void(0)"       onclick="botaoFechar()"    class="bar-item button  large   padding-16"> FECHAR</a>
+            <a  href="#Home"                    onclick="botaoFechar()"    class="bar-item button">    HOME         </a>
+            <a  href="#servicos"                onclick="botaoFechar()"    class="bar-item button">    SERVIÇOS     </a>
+            <a  href="SobreNos/index.html"      onclick="botaoFechar()"    class="bar-item button">    SOBRE NÓS    </a>
+            <a  href="#Contato"                 onclick="botaoFechar()"    class="bar-item button">    CONTATO      </a>
+            <a  href="indexEnglish.html"        onclick="botaoFechar()"    class="bar-item button">    ENGLISH      </a>
+        </nav>
+
+    </body>
+
+</html>
